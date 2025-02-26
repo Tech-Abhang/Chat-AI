d86a76a07456fab2ec1301b94153449f5c6739f3
Abhang Pawar <abhangpawar03@gmail.com>
2025-02-26 20:47:53 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ function App() {
 
   return (
     <>
-      <div className="main">
+      <div className="flex flex-col justify-between h-screen overflow-y-hidden pt-3">
         <div className="name">
-          <h1>Chat-AI</h1>
+          <h1 className='text-3xl'>Chat-AI</h1>
         </div>
 
-        <div className="output">
-          <pre>{output}</pre>
+        <div className="output p-3">
+          <p>{output}</p>
         </div>
 
-        <div className="search">
-          <textarea onChange={(e) => setInput(e.target.value)} value={input} className='textarea'></textarea>
-          <button onClick={generateAns}>Generate</button>
+        <div className="search flex justify-center gap-5">
+          <textarea onChange={(e) => setInput(e.target.value)} value={input} className='rounded-4xl w-200 mb-5 h-12 px-4 py-2 text-[18px] bg-[#3d3d3a] border-0' placeholder='ask anything'></textarea>
+          <button onClick={generateAns} className=' rounded-4xl p-2 h-13.5 bg-[#3d3d3a] text-[#a6a39a]' >send</button>
         </div>
       </div>
     </>
